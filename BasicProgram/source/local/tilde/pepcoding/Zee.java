9a939a36aca7afc6ccda362d7d0daa2fd88cc7ff
mrityunjay9693 <mkaykumar123@gmail.com>
2023-09-26 13:50:55 +0530

    Zee program done

---
+++ BasicProgram/source/local/tilde/pepcoding/Zee.java
@@ -0,0 +1,17 @@
+package BasicProgram.source.local.tilde.pepcoding;
+
+public class Zee {
+    public static void main(String[] args){
+        
+        System.out.println(" * * * * * ");
+        
+        System.out.println("       *   ");
+        
+        System.out.println("     *     ");
+        
+        System.out.println("   *       ");
+        
+        System.out.println(" * * * * * ");
+        
+    }
+}
