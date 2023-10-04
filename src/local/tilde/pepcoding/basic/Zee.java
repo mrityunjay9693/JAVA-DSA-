d34cb4b37f37a6b23e042df04efbf3964a683e50
mrityunjay9693 <mkaykumar123@gmail.com>
2023-10-04 16:14:20 +0530

    printing "Z" using System.out.println()

---
+++ src/local/tilde/pepcoding/basic/Zee.java
@@ -0,0 +1,11 @@
+package local.tilde.pepcoding.basic;
+
+public class Zee {
+    public static void main(String[] args) {
+        System.out.println("*****");
+        System.out.println("   *");
+        System.out.println("  *");
+        System.out.println(" *");
+        System.out.println("*****");
+    }
+}
