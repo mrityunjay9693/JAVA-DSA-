188f35687c02d469df3ad50d6d13cf5ffb2209d2
mrityunjay9693 <mkaykumar123@gmail.com>
2023-10-04 16:19:54 +0530

    Program to print 'Z' without loops

--- src/local/tilde/pepcoding/basic/Zee.java
+++ src/local/tilde/pepcoding/basic/Zee.java
@@ -2,6 +2,7 @@ package local.tilde.pepcoding.basic;
 
 public class Zee {
     public static void main(String[] args) {
+        System.out.println("Print 'Z' without loops:");
         System.out.println("*****");
         System.out.println("   *");
         System.out.println("  *");
