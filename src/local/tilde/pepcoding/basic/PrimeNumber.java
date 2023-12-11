662c5bc691f5f4e08c894a53cff6cefa828bfcd2
mrityunjay9693 <mkaykumar123@gmail.com>
2023-12-11 12:35:12 +0530

    program updates.

--- src/local/tilde/pepcoding/basic/PrimeNumber.java
+++ src/local/tilde/pepcoding/basic/PrimeNumber.java
@@ -9,17 +9,17 @@ public class PrimeNumber {
         int number = scan.nextInt();
         int flag = 1; // 1 -> prime
 
-        for (int i = 2; i < number; i++) {
+        for (int i = 2; i < (number / 2); i++) {
             if (number % i == 0) {
                 flag = 0;
                 break;
             }
         }
-        System.out.println("flag:"+flag);
+        System.out.println("flag:" + flag);
         if (flag == 0) {
             System.out.println(number + " is not a prime number");
         } else {
-            System.out.println(number+" is a prime number");
+            System.out.println(number + " is a prime number");
         }
         scan.close();
     }
