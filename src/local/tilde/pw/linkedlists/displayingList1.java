ffb1be67548bc56c17a1f1a99614689f3c9a0612
mrityunjay9693 <mkaykumar123@gmail.com>
2023-12-27 08:09:34 +0530

    displaying linked list using while loop

--- src/local/tilde/pw/linkedlists/displayingList1.java
+++ src/local/tilde/pw/linkedlists/displayingList1.java
@@ -10,7 +10,7 @@ public class displayingList1 {
         Node n3 = new Node(3);
         Node n4 = new Node(9);
         Node n5 = new Node(7); // n5 -> tail
-
+        
         // Establishing the relation between nodes
         // daat: 5 4 3 9 7
         n1.next = n2; // 5 -> 4 3 9 7
