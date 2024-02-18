91b665b945f6c116e03f84633960c38cbcd01e81
mrityunjay9693 <mkaykumar123@gmail.com>
2024-02-18 11:50:36 +0530

    length of an array

---
+++ src/local/tilde/pepcoding/arrays/LengthOfArray.java
@@ -0,0 +1,11 @@
+package local.tilde.pepcoding.arrays;
+
+public class LengthOfArray {
+    public static void main(String[] args) {
+        int arr[] = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
+        // length property
+        int lengthOfArray = arr.length;
+        System.out.println("Length of an Array : " + lengthOfArray);
+    }
+
+}
