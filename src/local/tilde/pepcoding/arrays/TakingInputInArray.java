4960036ad4bc8a36de10d030d4be6799dd45d4d1
mrityunjay9693 <mkaykumar123@gmail.com>
2024-02-29 22:23:11 +0530

    taking input in array

---
+++ src/local/tilde/pepcoding/arrays/TakingInputInArray.java
@@ -0,0 +1,15 @@
+package local.tilde.pepcoding.arrays;
+
+import java.util.Scanner;
+
+public class TakingInputInArray {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        int length = scan.nextInt();
+        int[] arr = new int[length];
+
+        for(int i = 0; i < length; i++){
+            arr[i] = scan.nextInt();
+        }
+    }   
+}
