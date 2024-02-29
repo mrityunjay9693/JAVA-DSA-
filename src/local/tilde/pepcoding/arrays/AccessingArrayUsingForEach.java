ad7eac4fc056927a71fa0587119ea910e71d44a6
mrityunjay9693 <mkaykumar123@gmail.com>
2024-02-29 17:53:26 +0530

    accessing array elements.

---
+++ src/local/tilde/pepcoding/arrays/AccessingArrayUsingForEach.java
@@ -0,0 +1,11 @@
+package local.tilde.pepcoding.arrays;
+
+public class AccessingArrayUsingForEach {
+    public static void main(String[] args){
+        int[] arr = {1,2,3,4,5,6,7,8};
+
+        for(int value : arr){
+            System.out.println(value);
+        }
+    }    
+}
