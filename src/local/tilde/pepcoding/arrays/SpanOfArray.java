a7219f766c4e82788900bea090224d5078533d1d
mrityunjay9693 <mkaykumar123@gmail.com>
2024-03-02 21:02:44 +0530

    Problem 1 and Problem 2 is added in notes.

---
+++ src/local/tilde/pepcoding/arrays/SpanOfArray.java
@@ -0,0 +1,22 @@
+/**
+ * Problem: Span Of Array
+            1.You are given a number "n", representing the count of elements.
+            2.You are given n numbers.
+            3.You are required to find the span of inputs. Span is defined as difference of maximum value and
+              minimum value.
+            4.Return span value
+ * Input Format: n (total numbers of element in an array)
+                 n1, n2, n3, n4, n5, n6, ... nn.
+ * Output Format: spanValue ( maximum value - minimum value)
+ * Constraints: 1 <= n <= 10^4 and 0 <= n1, n2, n3, n4, n5, n6, ... n elements <= 10^9
+ * Sample Inputs: 6 (n)
+                  15, 30, 40, 4, 11, 9
+ * Sample OUtput: 36 (40 - 4)
+ * Span value = Maximum value - Minimum value
+ */
+
+package local.tilde.pepcoding.arrays;
+
+public class SpanOfArray {
+    
+}
