d57cd0d127dd994b0042ea071e77f12df2d7ec85
mrityunjay9693 <mkaykumar123@gmail.com>
2024-03-03 21:31:25 +0530

    main method done.

---
+++ src/local/tilde/pepcoding/arrays/InverseOfArray.java
@@ -0,0 +1,34 @@
+/**
+ * Problem: Inverse Of An Array
+            1.You are given a number "n",representing the size of array arr.
+            2.You are given n numbers, representing elements of array arr.
+            3.You are required to calculate the inverse of array array arr. 
+ * Constraints :0 <= n < 10^7. The only difference is the range of values is from 0 to n-1, instead of 1 to n.
+ * Sample Input: 5 (n : size of array arr)
+                 4, 0, 2, 3, 1
+ * Sample Output:1, 4, 2, 3, 0
+ */
+
+package local.tilde.pepcoding.arrays;
+
+import java.util.Scanner;
+
+public class InverseOfArray {
+    public static int[] inverseArray(int[] arr) {
+        
+        return null;
+    }
+
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        System.out.print("Enter size of array(n): ");
+        int n = scan.nextInt();
+        int[] arr = new int[n];
+
+        for (int i = 0; i < n; i++) {
+            arr[i] = scan.nextInt();
+        }
+        inverseArray(arr);
+        scan.close();    
+    }
+}
