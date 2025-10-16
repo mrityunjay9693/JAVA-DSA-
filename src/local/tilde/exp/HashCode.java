1b0a7bb13a0ebb73919e70cd5b8ceaa214d9a1d9
mrityunjay9693 <56473482+mrityunjay9693@users.noreply.github.com>
2025-10-16 18:13:46 +0530

    hash code program

---
+++ src/local/tilde/exp/HashCode.java
@@ -0,0 +1,11 @@
+package local.tilde.exp;
+
+public class HashCode {
+    public static void main(String[] args) {
+        HashCode hashcode = new HashCode();
+        HashCode hashCode1 = new HashCode();
+
+        System.out.println("Hash Code " + hashcode + " : " + hashcode.hashCode());
+        System.out.println("Hash Code " + hashcode + " : " + hashCode1.hashCode());
+    }
+}
